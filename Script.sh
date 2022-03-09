bbee66c9aafefd07d47937b7f65ebe103eb2e7c5
Martin <97765238+Mis125@users.noreply.github.com>
2022-03-09 23:43:06 +0100

    Add files via upload

---
+++ Script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+interfaces=()
+for web in $(ifconfig | cut -d ' ' -f1 | tr ':' '\n' | awk NF); do
+	if [ "$web" != "lo" ]; then
+		interfaces+=("$web")
+	fi
+done
+
+
+for inter in ${interfaces[@]}
+do
+echo $inter
+echo "Status:  $(cat /sys/class/net/$inter/operstate)"
+echo "MAC: $(cat /sys/class/net/$inter/address)"
+echo "IP: $(hostname -I)"
+
+
+done
